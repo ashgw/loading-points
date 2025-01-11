a665710a9ef9b01259e5526f50a31177f85a1ac6
AshGw <ashrefgw@proton.me>
2025-01-11 19:14:19 +0100

    removed the branded type

--- src/index.tsx
+++ src/index.tsx
@@ -6,13 +6,8 @@ const DEFAULT = {
   GLOW_COLOR: 'rgb(155, 46, 199)', // Purple
 } as const;
 
-declare const __s: unique symbol;
-type Brand<N, T> = T & {
-  [__s]: N;
-};
-
-export type RGB = Brand<'RBG', string>;
-export type Pixel = Brand<'Pixel', string>;
+export type RGB = string;
+export type Pixel = string;
 
 type DotProps = {
   glowColor?: RGB;
@@ -21,15 +16,15 @@ type DotProps = {
 };
 
 const Dot = styled.div<DotProps>`
-  width: ${(props) => props.circleSize || DEFAULT.CIRCLE_SIZE};
-  height: ${(props) => props.circleSize || DEFAULT.CIRCLE_SIZE};
+  width: ${(props) => props.circleSize ?? DEFAULT.CIRCLE_SIZE};
+  height: ${(props) => props.circleSize ?? DEFAULT.CIRCLE_SIZE};
   border-radius: 50%;
   display: inline-block;
   margin: 0.5rem;
-  background-color: ${(props) => props.glowColor || DEFAULT.GLOW_COLOR};
-  box-shadow: 0 0 20px ${(props) => props.glowColor || DEFAULT.GLOW_COLOR};
+  background-color: ${(props) => props.glowColor ?? DEFAULT.GLOW_COLOR};
+  box-shadow: 0 0 20px ${(props) => props.glowColor ?? DEFAULT.GLOW_COLOR};
   animation: loading 1.5s ease-in-out infinite;
-  animation-delay: ${(props) => `${props.delay}s` || '0s'};
+  animation-delay: ${(props) => `${props.delay ?? '0'}s`};
 
   @keyframes loading {
     0%,
@@ -43,20 +38,17 @@ const Dot = styled.div<DotProps>`
     50% {
       transform: scale(1);
       box-shadow:
-        0 0 20px ${(props) => props.glowColor || DEFAULT.GLOW_COLOR},
-        0 0 30px ${(props) => props.glowColor || DEFAULT.GLOW_COLOR},
-        0 0 40px ${(props) => props.glowColor || DEFAULT.GLOW_COLOR};
+        0 0 20px ${(props) => props.glowColor ?? DEFAULT.GLOW_COLOR},
+        0 0 30px ${(props) => props.glowColor ?? DEFAULT.GLOW_COLOR},
+        0 0 40px ${(props) => props.glowColor ?? DEFAULT.GLOW_COLOR};
     }
   }
 `;
 
-type DotsProps = Omit<DotProps, 'delay'>;
-
-export interface LoaderProps
-  extends React.ButtonHTMLAttributes<HTMLDivElement>,
-    DotsProps {}
-
-const Loading: React.FC<LoaderProps> = ({ glowColor, circleSize }) => {
+const Loading: React.FC<Omit<DotProps, 'delay'>> = ({
+  glowColor,
+  circleSize,
+}) => {
   const renderDivs = () => {
     return (
       <>
